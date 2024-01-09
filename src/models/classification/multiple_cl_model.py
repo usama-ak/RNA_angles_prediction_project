06ffc8a8adee7e2d3917419d68467ecb73a3d7d8
usama-ak <usama.akhtar@outlook.fr>
2024-01-09 20:56:42 +0100

    Create multiple_cl_model.py

---
+++ src/models/classification/multiple_cl_model.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+
+# Define the RNN model
+class AnglePredictionRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_layers=4, dropout=0.5):
+        super(AnglePredictionRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        # Normalization layer
+        #self.norm = nn.LayerNorm(input_size)        
+        # LSTM layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers=num_layers, batch_first=True)
+        # Dropout layer
+        self.dropout = nn.Dropout(dropout)
+        # Fully connected layer
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x, mask):
+        # Initialize hidden and cell states for LSTM
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        # LSTM forward pass
+        output, _ = self.lstm(x, (h0, c0))
+        # Apply mask to ignore padded values
+        mask_tr = mask.transpose(1, 2)  # Squeeze and transpose to move the singleton dimension to the third position
+        expanded_mask = mask_tr.expand(-1,-1,output.size(2))  # Expand to match the output tensor
+        output = output * expanded_mask  # Apply the mask directly
+        # Apply dropout
+        output = self.dropout(output)
+        # Fully connected layer
+        output = self.fc(output)
+        return output
