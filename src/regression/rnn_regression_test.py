a82aa79622e55de11100519c5f1da65a727c1855
usama-ak <usama.akhtar@outlook.fr>
2024-01-09 20:48:15 +0100

    Update rnn_regression_test.py

--- src/regression/rnn_regression_test.py
+++ src/regression/rnn_regression_test.py
@@ -1,10 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.callbacks import EarlyStopping
 import os
-import sys
-from os.path import dirname, abspath
-parent_dir = dirname(dirname(abspath(__file__)))
-sys.path.append(parent_dir)
 from utils.evaluate_models import print_evaluation_metrics, plot_angles, plot_learning_curves
 from utils.datapreprocess_reg import prepare_data, convert_to_tensors
 from models.regression.regression_model import create_rnn_model
