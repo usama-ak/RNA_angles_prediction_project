861d724e9a229f5e9430518bbea5a89cd0dfc90e
usama-ak <usama.akhtar@outlook.fr>
2024-01-09 19:29:53 +0100

    Update rnn_regression.py

--- src/rnn_regression.py
+++ src/rnn_regression.py
@@ -5,10 +5,11 @@ import tensorflow as tf
 from tensorflow.keras.callbacks import EarlyStopping
 import os
 
-base_path = os.path.dirname(os.path.realpath(__file__))
+current_dir = os.path.dirname(os.path.abspath(__file__))
+data_folder = os.path.join(current_dir, '..', 'data')
 
-train_path = os.path.join(base_path, "data", "AngleFilesOutput")
-test_path = os.path.join(base_path, "data", "AngleFilesTestOutput")
+train_path = os.path.join(data_folder, 'AngleFilesOutput')
+test_path = os.path.join(data_folder, 'AngleFilesTestOutput')
 
 trainseq_encoded, trainangles_encoded, trainmasks = prepare_data(train_path)
 testseq_encoded, testangles_encoded, testmasks = prepare_data(test_path)
@@ -18,11 +19,7 @@ train_seq_tensor, train_angles_tensor, train_masks_tensor = convert_to_tensors(t
 test_seq_tensor, test_angles_tensor, test_masks_tensor = convert_to_tensors(testseq_encoded, testangles_encoded, testmasks)
 
 
-early_stopping = EarlyStopping(monitor='val_loss',  # Monitor validation loss
-                               patience=5,           # Number of epochs with no improvement after which training will be stopped
-                               restore_best_weights=True,  # Restore model weights from the epoch with the best value of the monitored quantity
-                               verbose=1)           # Print messages about early stopping to the console
-
+early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True, verbose=1)
 
 input_shape = (395,4)
 rnn_model = create_rnn_model(input_shape)
@@ -34,13 +31,9 @@ history = rnn_model.fit(train_seq_tensor,
                         batch_size=32,
                         callbacks=[early_stopping])
 
-
-
 evaluation = rnn_model.evaluate(test_seq_tensor, test_angles_tensor, sample_weight=test_masks_tensor)
 y_pred = rnn_model.predict(test_seq_tensor)
 y_true_np = test_angles_tensor.numpy().flatten()
 y_pred_np = y_pred.flatten()
 print_evaluation_metrics(evaluation, test_angles_tensor, y_pred)
 plot_angles(y_true_np, y_pred_np, title="Expected vs Predicted Angles")
-
-
