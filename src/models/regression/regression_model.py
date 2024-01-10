2ad5fe21886254ac02624e63fafb63bb8692d12f
MrAli1582 <139212688+MrAli1582@users.noreply.github.com>
2024-01-10 13:47:16 +0100

    Update regression_model.py

--- src/models/regression/regression_model.py
+++ src/models/regression/regression_model.py
@@ -2,15 +2,17 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, TimeDistributed, GRU, Dropout
 from tensorflow.keras.optimizers import Adam
 
+## We create a regression model 
 def create_rnn_model(input_shape):
     model = Sequential()
-    # LSTM layer with 64 units and return_sequences=True to maintain sequence length
+    ## Then we add three layers to the model : GRU, Dropout (to drop some of the neurons) and LSTM
     model.add(GRU(64, input_shape=input_shape, return_sequences=True))
     model.add(Dropout(0.1))
     model.add(LSTM(32, return_sequences=True))
-    # TimeDistributed layer to apply Dense layer to each time step independently
-    model.add(TimeDistributed(Dense(1, activation='linear')))  # Outputting an angle per nucleotide
-    
+    ## We also add a TimeDistributed layer to apply Dense layer to each time step independently (so that we can output an angle value per nucleotide)
+    model.add(TimeDistributed(Dense(1, activation='linear'))) 
+
+    ## Then we define hyperparameters (learning rate, optimizer)
     custom_learning_rate = 0.001
 
     optimizer = Adam(learning_rate=custom_learning_rate)
