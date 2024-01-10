cfa40cbad620b44f3004d3a8d4757b8c373aad89
MrAli1582 <139212688+MrAli1582@users.noreply.github.com>
2024-01-10 14:00:39 +0100

    Update multiple_cl_model.py

--- src/models/classification/multiple_cl_model.py
+++ src/models/classification/multiple_cl_model.py
@@ -1,32 +1,33 @@
 import torch
 import torch.nn as nn
 
+## We create a class AnglePredictionRNNmulti where we create the multi class instances model that will be used to predict angles
 class AnglePredictionRNNmulti(nn.Module):
     def __init__(self, input_size=4, hidden_size=64, output_size=40, num_layers=4, dropout=0.5):
         super(AnglePredictionRNNmulti, self).__init__()
+        ## We define hidden_size and number of layers that will be associated to the model
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         # Normalization layer
         #self.norm = nn.LayerNorm(input_size)        
-        # LSTM layers
+        ## LSTM layer of our model
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers=num_layers, batch_first=True)
-        # Dropout layer
+        ## Dropout layer of our model
         self.dropout = nn.Dropout(dropout)
-        # Fully connected layer
+        ## Fully connected layer of our model
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x, mask):
-        # Initialize hidden and cell states for LSTM
+        ## Here we initialize the hidden and cell states for LSTM
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
         c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-        # LSTM forward pass
         output, _ = self.lstm(x, (h0, c0))
-        # Apply mask to ignore padded values
+        ## Then we apply masks to ignore padded values by tansposing and expanding them so that they fit the output tensor
         mask_tr = mask.transpose(1, 2)  
         expanded_mask = mask_tr.expand(-1,-1,output.size(2))  
         output = output * expanded_mask 
-        # Apply dropout
+        ## Applying dropout
         output = self.dropout(output)
-        # Fully connected layer
+        ## Applying Fully connected layer
         output = self.fc(output)
         return output
