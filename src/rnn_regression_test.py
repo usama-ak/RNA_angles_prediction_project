e7a265974c8ae870306fe68ff90878cf1d0068b3
MrAli1582 <139212688+MrAli1582@users.noreply.github.com>
2024-01-10 17:36:16 +0100

    Update rnn_regression_test.py

--- src/rnn_regression_test.py
+++ src/rnn_regression_test.py
@@ -5,6 +5,7 @@ from utils.evaluate_models import print_evaluation_metrics, plot_angles, plot_le
 from utils.datapreprocess_reg import prepare_data, convert_to_tensors
 from models.regression.regression_model import create_rnn_model
 
+## Defining the directories to generate an absolute path for the train and the test data
 current_dir = os.path.dirname(os.path.abspath(__file__))
 data_folder = os.path.join(current_dir, '..', 'data')
 train_path = os.path.join(data_folder, 'AngleFilesOutput')
@@ -16,10 +17,16 @@ testseq_encoded, testangles_encoded, testmasks = prepare_data(test_path)
 train_seq_tensor, train_angles_tensor, train_masks_tensor = convert_to_tensors(trainseq_encoded, trainangles_encoded, trainmasks)
 test_seq_tensor, test_angles_tensor, test_masks_tensor = convert_to_tensors(testseq_encoded, testangles_encoded, testmasks)
 
+## Defining early stopping callback to prevent overfitting
 early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True, verbose=1)
 
+## Defining the input shape of the model
 input_shape = (395,4)
+
+## Here we use the RNN model that we created previously on the test data with the input size previously defined
 rnn_model = create_rnn_model(input_shape)
+
+## Then we train the model
 history = rnn_model.fit(train_seq_tensor,
                         train_angles_tensor,
                         sample_weight=train_masks_tensor,
@@ -28,7 +35,9 @@ history = rnn_model.fit(train_seq_tensor,
                         batch_size=32,
                         callbacks=[early_stopping])
 
+## Next, we evaluate the model on the test data
 evaluation = rnn_model.evaluate(test_seq_tensor, test_angles_tensor, sample_weight=test_masks_tensor)
+## we make predictions on the testing data
 y_pred = rnn_model.predict(test_seq_tensor)
 y_true_np = test_angles_tensor.numpy().flatten()
 y_pred_np = y_pred.flatten()
