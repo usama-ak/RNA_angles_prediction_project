ecd6ec7f46bc4611c9e501358fbcefb398788e9e
MrAli1582 <139212688+MrAli1582@users.noreply.github.com>
2024-01-10 17:09:44 +0100

    Update input_preprocess.py

--- src/utils/input_preprocess.py
+++ src/utils/input_preprocess.py
@@ -1,5 +1,6 @@
 from Bio import SeqIO
 
+## First we define a function that will extract the sequences from a FASTA file using SeqIO
 def get_sequences_from_fasta(fasta_file):
     sequences = []
     for record in SeqIO.parse(fasta_file, "fasta"):
@@ -8,25 +9,25 @@ def get_sequences_from_fasta(fasta_file):
 
 import torch
 
+## Then we have a second function for which one-hot enconding, padding and masking will be done for the sequence
 def one_hot_encode_sequence(sequence):
-    # Define the mapping for one-hot encoding
+    ## Here we define the mapping for one-hot encoding
     mapping = {'A': 1, 'C': 2, 'G': 3, 'U': 4}
     
-    # Convert the sequence to a list of integers using the mapping
+    ## Then, converting the sequence to a list of integers using the mapping
     sequence_int = [mapping.get(base, -1) for base in sequence]
-    # Pad sequence to the maximum length
+    ## Padding the sequence to a maximum length so that it fits with the input size of the model
     max_length = 395
     padded_sequence_int = sequence_int + [0] * (max_length - len(sequence_int))
 
-    # Perform one-hot encoding manually
     vocab_size = 4
     encoded_sequence = torch.zeros(max_length, vocab_size)
-    mask = torch.zeros(max_length)
+    mask = torch.zeros(max_length) ## Generating masks for the sequence
 
     for i, base_idx in enumerate(padded_sequence_int):
-        if base_idx != 0:  # Ignore padding value
-            encoded_sequence[i, base_idx - 1] = 1  # Assuming the base index starts from 1
-            mask[i] = 1  # Set mask to 1 for real nucleotide
+        if base_idx != 0:  
+            encoded_sequence[i, base_idx - 1] = 1  
+            mask[i] = 1  ## Here we set the mask value to 1 for real nucleotides
 
     return encoded_sequence, mask
 
